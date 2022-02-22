ca3a169cffa4405a9fef4f68de0adc91d63a41bd
Diana <d.anavasinkina@gmail.com>
2022-02-22 11:34:13 +0300

    r

--- main.js
+++ main.js
@@ -48,11 +48,11 @@ console.log("Сэм" + 5);
 
 console.log("Сэм" - 5);
 // Предположение: Сэм
-// Фактический: Nan
+// Фактический: NaN
 
 console.log(99 * "шары");
-// Предположение: Nan
-// Фактический: Nan
+// Предположение: NaN
+// Фактический: NaN
 
 //Задача 2
 console.log("Задача 2");
@@ -73,8 +73,8 @@ console.log("Задача 3");
 
 let Cels = 25;
 let F = 451;
-console.log(`${Cels}` + "\xB0C", "соответствует", `${Cels*33.8}` + "\xB0F");
-console.log(`${F}` + "\xB0F", "соответствует", `${F/33.8}` + "\xB0C");
+console.log(`${Cels}\xB0C соответствует ${Cels*33.8}\xB0F`);
+console.log(`${F}\xB0F соответствует ${F/33.8}\xB0C`);
 
 //Задача 4
 console.log("Задача 4");
@@ -86,12 +86,12 @@ alert(year%4 == 0 ? (year%100 != 0 ? "False" : "True") : "False");
 console.log("Задача 5");
 
 
-let first = prompt("Введите первое число");
-let second = prompt("Введите второе число");
+let first = +prompt("Введите первое число");
+let second = +prompt("Введите второе число");
 
 alert("Проверим равно ли одно из чисел 10 или сумма чисел равна 10");
 
-if (first == 10 || second == 10 || Number(first) + Number(second) == 10) {
+if (first == 10 || second == 10 || first + second == 10) {
     alert("Истина");
 }
 else alert("Ложь");
@@ -99,7 +99,7 @@ else alert("Ложь");
 //Задача 6
 let numSheet = prompt("Введите любое натуральное число");
 
-let str = " ";
+let str = "";
 for (let i = 1; i <= numSheet; i++){
     str += i + " овечка..."
 }
@@ -138,9 +138,10 @@ console.log("Задача 9");
 let d = 5;
 let e = 8;
 let f = -4;
+let t;
 
 if (d > f) {
-    let t = d;
+    t = d;
     d = f;
     f = t;
 }
