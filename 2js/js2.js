d49c9e60c92aebc6d3d1bcfa3060c9b37be452a2
Diana <d.anavasinkina@gmail.com>
2022-09-25 12:23:02 +0300

    first commit

---
+++ 2js/js2.js
@@ -0,0 +1,144 @@
+//Задача 1
+console.log("Задача 1. Функция absValue");
+function absValue(N) {
+    if (N < 0) {
+        return -N;
+    }
+    return N;
+}
+
+
+//Задача 2
+console.log("Задача 2. isPalindrome");
+function isPalindrome(word) {
+    let arr = []
+    //word.split('');
+    for (let char of word) {
+        arr.push(char);
+    }
+    for (let i = 0; 2 * i <= arr.length; i++) {
+        if (arr[i] == arr[arr.length - 1 - i])
+            return console.log("палиндром");
+        else
+            return console.log("не палиндром");
+    }
+
+
+}
+
+//Задача3
+console.log("Задача 3. matrixAddition");
+/*
+function checkMatrix(arr1, arr2) {
+    if (arr1.length !== arr2.length)
+        console.log('Матрицы не равны!');
+    else {
+        for (let n=1; n < arr1.length; n++){
+            if ((arr1[n-1].length !== arr1[n])||(arr2[n-1].length !== arr2[n])||(arr1[n-1].length !== arr2[n-1].length))
+            console.log('Матрицы не равны!');
+        }
+    }
+}
+*/
+
+function checkMatrix(arr1, arr2) {
+    if (arr1.length == arr2.length)
+        {
+        for (let n=1; n < arr1.length; n++) {
+            if ((arr1[n-1].length == arr1[n].length)&&(arr2[n-1].length && arr2[n].length)&&(arr1[n-1].length == arr2[n-1].length)) {
+                //console.log('матрицы равны');
+                return 1;
+            }
+            else return 0;
+        }
+        }
+    else 
+        return 0;
+    }
+
+
+function matrixAddition(arr1, arr2) {
+    let arr3 = [];
+    //console.log(arr1[0][1] + arr2[0][1]);
+    
+    
+   
+   
+    //if ((arr1[0].length == arr1[1].length)&&(arr2[0].length==arr2[1].length)&&(arr1[0].length==arr2[0].length)) {
+    if (checkMatrix(arr1, arr2)) {  
+        for (let i=0; i<=arr1.length-1; i++) {
+                for (let k=0; k<=arr1[0].length-1; k++){
+                    arr3.push(arr1[i][k] + arr2[i][k]);
+                }
+            }
+        
+        for (let i=0; i<= arr3.length-1; i+=2)
+            console.log(arr3[i] + '\t', arr3[i+1] + '\n');
+    }
+    else {
+        return console.log('Матрицы не равны! Сложение невозможно');
+    }
+    
+}
+let matrix1 = [[1,2], [0,0], [5,3]], matrix2 = [[2,3], [7,8], [8,7]];
+console.log(matrix1.length);
+matrixAddition(matrix1, matrix2);
+
+//Задача 4
+console.log("Задача 4");
+let student = {
+    group: 201,
+    last_name: "Иванов",
+    first_name: "Иван"
+};
+console.log(`Список свойств: `, Object.keys(student));
+console.log(`Студент ${student.first_name} ${student.last_name} учится в ${student.group} группе`);
+
+
+//Задача 5 попытка 2
+let elements = document.querySelectorAll('.slide');
+let go = document.querySelector('.slider-next');
+let back = document.querySelector('.slider-previous');
+let currentSlide;
+let oldSlide;
+let i = 0;
+
+let changeIndexGo = function () {
+    i++;
+    if (i <= 2) {
+        oldSlide = elements[i - 1];
+        currentSlide = elements[i];
+    }
+    else {
+        oldSlide = elements[i - 1];
+        i = 0;
+        currentSlide = elements[i];
+    }
+}
+
+let changeIndexBack = function () {
+    i--;
+    if (i >= 0) {
+        oldSlide = elements[i + 1];
+        currentSlide = elements[i];
+    }
+    else {
+        oldSlide = elements[i + 1];
+        i = 2;
+        currentSlide = elements[i];
+    }
+}
+
+go.addEventListener('click', function () {
+    changeIndexGo();
+    oldSlide.classList.add('hide');
+    currentSlide.classList.remove('hide');
+
+});
+
+back.addEventListener('click', function () {
+    changeIndexBack();
+    oldSlide.classList.add('hide');
+    currentSlide.classList.remove('hide');
+});
+
---
+++ 3js/js3.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+    console.log(scrollbar);
+    document.querySelector('input').addEventListener('click', function () {
+        document.body.style.overflow = 'hidden';
+        document.querySelector('.modal').style.marginLeft = scrollbar;
+    });
+    document.getElementById('close').addEventListener('click', function () {
+        document.body.style.overflow = 'visible';
+        document.querySelector('.modal').style.marginLeft = '0px';
+    });
+});
+
+let view = document.querySelector('.modal');//модальное окно
+
+//открытие окна по нажатию кнопки
+document.getElementById('open').onclick = function () {
+    view.classList.add('show');
+}
+
+//закрытие окна при нажатии кнопки
+document.getElementById('close').onclick = function () {
+    view.classList.remove('show');
+}
+
+//закрытие окна при нажатии вне его
+view.addEventListener('click', function (event) {
+    //var target = event.target;
+    var menu = document.querySelector('.modal-content');
+
+    if (!menu.contains(event.target)) {
+        view.classList.remove('show');
+        //console.log('work');
+    }
+});
+
+
+const email = document.getElementById("mail"),
+pass=document.getElementById("pass"),
+agree = document.getElementById("agree"),
+agreePers = document.getElementById("agree_pers");
+
+
+email.addEventListener('blur', function(event)
+{
+    switch(true) {
+        case email.validity.valueMissing:
+            email.setCustomValidity("Заполните поле!");
+            break;
+        case email.validity.typeMismatch:
+            email.setCustomValidity("Неправильный адрес электронной почты");
+            break;
+        default: 
+            email.setCustomValidity("");
+    }
+    email.reportValidity();
+}
+);
+
+
+pass.addEventListener('blur', function()
+{
+    switch(true) {
+        case pass.validity.valueMissing:
+            pass.setCustomValidity("Заполните поле!");
+            break;
+        case pass.validity.tooShort:
+            pass.setCustomValidity("Пароль слишком короткий! Не меньше 6 символов!");
+            break;
+        case pass.validity.tooLong:
+            pass.setCustomValidity("Пароль слишком длинный! Не больше 20 символов!");
+            break;
+        default: 
+            pass.setCustomValidity("");
+    }
+    pass.reportValidity();
+}
+);
+
+//отмена действия по умолчанию - отправка формы на сервер и вывод данных формы в консоль
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    
+    const data = {
+        email: email.value,
+        password: pass.value,
+        agreement: agree.checked,
+        agreement_person: agreePers.checked
+    }
+    console.table(data);
+}
+);
+ 
